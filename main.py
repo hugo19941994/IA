b3c13725e1ebc1197518548646164557eaf9483a
Hugo Ferrando Seage <hugoseage@gmail.com>
2015-11-17 13:47:02 +0100

    URL corregida, comprueba SHA1 de Tika, comprueba extensiones de CV

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # * Hugo Ferrando Seage
 # * Santiago Gualda Torrijos
 # * Cristian López-Ramos Rivera
-# TODO: Name Entity Recognition, Web service?, improve segmentation, HMM?, JSON or YAML export?, verify download
+# TODO: Name Entity Recognition, Web service?, improve segmentation, HMM?, JSON or YAML export?, choose mirror dynamically?, poner porcentaje descargar en vez de puntos
 
 import nltk
 import sys
@@ -13,14 +13,44 @@ import errno
 import urllib.request
 import shutil
 import os.path
+import hashlib
+import time
+import threading
 from nltk import *
+
+def mientrasDescarga():
+    spaces = 0
+    while run:
+        sys.stdout.write(" " * spaces + ".\r")
+        if spaces >= 10:
+            sys.stdout.write(" " * 11 + "\r")
+            spaces = 0
+        else:
+            spaces += 1
+        time.sleep(1)
+    return
 
 # Comprobar si Tika 1.11 existe en la carpeta
 if (not os.path.isfile("tika-app-1.11.jar")):
     # Se descarga si no está
-    print("Downloading Tika 1.11")
-    with urllib.request.urlopen("http://www.apache.org/dyn/closer.cgi/tika/tika-app-1.11.jar") as response, open("tika-app-1.11.jar", 'wb') as out_file:
+    print("Descargando Tika 1.11")
+
+    # Imprimimos mensaje por consola para que no parezca que el programa esta parado
+    run = True
+    t = threading.Thread(target=mientrasDescarga, daemon=True)
+    t.start()
+
+    with urllib.request.urlopen("http://ftp.cixug.es/apache/tika/tika-app-1.11.jar") as response, open("tika-app-1.11.jar", 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
+    run = False
+
+# Comprobar Tika con su SHA1
+with open("tika-app-1.11.jar", 'rb') as f:
+    if hashlib.sha1(f.read()).hexdigest() == "59cc7c4c48a6a41899ca282d925b2738d05a45a8":
+        print("Tika tiene SHA1 correcto")
+    else:
+        print("Tika no tiene SHA1 correcto\nBorra el archivo de Tika y prueba otra vez")
+        exit()
 
 # Descargar tokenizador de NLTK
 nltk.download("punkt")
@@ -56,7 +86,7 @@ def escribirtxt(datos, educacion, laboral, emails):
 path = 'cv/*.*'
 
 # Lectura de de los curriculums en formato pdf, html, Word y OpenOfice
-files = [f for f in glob.glob(path) if ".pdf" or ".html" or ".doc" or ".docx" or ".odt" in f]
+files = [f for f in glob.glob(path) if f.lower().endswith((".pdf", ".html", ".doc", ".docx", ".odt"))]
 
 for name in files:  # name = Nombre de curriculum
     try:
