b5ed0aa8d2ffbbfc5d93301f32e420b127f762e8
sgualdat <sgualdat@gmail.com>
2015-11-21 12:00:15 +0100

    Interfaz
    No esta vinculada al main, solo devuelve el path de un curriculum o de
    la carpeta de los curriculums

---
+++ window.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter.filedialog import *
+import tkinter.messagebox
+
+top=tkinter.Tk()
+top.geometry("250x200")
+top.title("Clasificador de CV")
+logo=PhotoImage(file="logo_everis.png")
+
+def load_file():
+        fname_1 = askopenfilename(filetypes=(("PDF", "*.pdf"),
+                                           ("HTML files", "*.html;*.htm"),
+                                           ("Text files", "*.docx;*.txt;*.odt"),
+                                           ("All files", "*.*") ))
+        if fname_1:
+            try:
+                print(fname_1)
+            except:                     # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % fname)
+            return
+def load_folder():
+        fname_2 = askdirectory()
+        if fname_2:
+            try:
+                print(fname_2)
+            except:                     # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % fname)
+            return
+
+label_3=Label(top, image=logo)
+label_1=Label(top, text="CV especifico")
+label_2=Label(top, text="Carpeta de CV")
+
+button_1=Button(top, text="Clasificar")
+button_2=Button(top, text="Ir a la carpeta")
+button_3=Button(top, text="Buscar", command=load_file)
+button_4=Button(top, text="Buscar", command=load_folder)
+
+label_3.grid(columnspan=3)
+label_1.grid(row=1, column=0)
+label_2.grid(row=2, column=0)
+button_3.grid(row=1, column=1)
+button_4.grid(row=2, column=1)
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+
+
+
+top.mainloop()
+
