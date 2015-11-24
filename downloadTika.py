2add00e4d27ef6915678296168c12b71be30ec1a
clopezramos <clopezdist@gmail.com>
2015-11-24 14:03:49 +0100

    Mostrar proceso de descarga, version alternativa
    En vez de tener un hilo que vaya imprimiendo cada 2 segundos el estado de la descarga, descargar por trozos y mostrar cada 5% el estado.

--- downloadTika.py
+++ downloadTika.py
@@ -1,68 +1,45 @@
 import urllib.request as urllib2
 import hashlib
-import time
-import threading
-
-run = True
-status = ""
 
 
-def mientrasDescarga():
-    while run:
-        print(status, end="")
-        time.sleep(2)
-    return
+def chunk_report(bytes_so_far, chunk_size, total_size):
+    percent = float(bytes_so_far) / total_size
+    percent = round(percent*100, 2)
+    if percent % 5 == 0:
+        print("Descargados %d de %d bytes (%0.2f%%)\r" % (bytes_so_far, total_size, percent))
+
+    if bytes_so_far >= total_size:
+        print('\nDescarga Completa\n')
 
 
-def download(url):
+def chunk_read(url, chunk_size=8192, report_hook=None):
     urlresponse = urllib2.urlopen(url)
+    meta = urlresponse.info()
+    meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
+    meta_length = meta_func("Content-Length")
+    total_size = None
+    if meta_length:
+        total_size = int(meta_length[0])
+    bytes_so_far = 0
 
-    # Obtención del nombre del archivo (última parte de la url)
-    filename = url.split('/')[-1]
-    if not filename:
-        filename = 'downloaded file'
+    while True:
+        chunk = urlresponse.read(chunk_size)
+        bytes_so_far += len(chunk)
 
-    # Escritura de archivo en modo wb (write binary mode)
-    with open(filename, 'wb') as f:
-        # Obtención de la longitud de archivo
-        meta = urlresponse.info()
-        meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
-        meta_length = meta_func("Content-Length")
-        file_size = None
-        if meta_length:
-            file_size = int(meta_length[0])
+        if not chunk:
+            break
 
-        print("Descargando: {0} Bytes: {1}".format(url, file_size))
+        if report_hook:
+            report_hook(bytes_so_far, chunk_size, total_size)
 
-        file_size_dl = 0
-        block_sz = 8192
-        statusthread = threading.Thread(target=mientrasDescarga, daemon=True)
-        statusthread.start()
-        while True:
-            buffer = urlresponse.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            f.write(buffer)
-
-            status = "{0:16}".format(file_size_dl)
-            if file_size:
-                status += "   [{0:6.2f}%]".format(file_size_dl * 100 / file_size)
-            status += chr(13)
-        run = False
-        print()
-
-    # Comprobar Tika con su SHA1
-    with open("tika-app-1.11.jar", 'rb') as f:
-        if hashlib.sha1(f.read()).hexdigest() == "59cc7c4c48a6a41899ca282d925b2738d05a45a8":
-            print("Tika tiene SHA1 correcto")
-        else:
-            print("Tika no tiene SHA1 correcto\nBorra el archivo de Tika y prueba otra vez")
-
-    return filename
+    return bytes_so_far
 
 
-url = "http://ftp.cixug.es/apache/tika/tika-app-1.11.jar"
-name = download(url)
-print(name)
+chunk_read('http://ftp.cixug.es/apache/tika/tika-app-1.11.jar', report_hook=chunk_report)
+
+# Comprobar Tika con su SHA1
+with open("tika-app-1.11.jar", 'rb') as f:
+    if hashlib.sha1(f.read()).hexdigest() == "59cc7c4c48a6a41899ca282d925b2738d05a45a8":
+        print("SHA1 correcto")
+    else:
+        print("SHA1 incorrecto\nBorra el archivo y prueba otra vez")
